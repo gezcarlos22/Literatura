e65ddec91a6f3d4754b0b55cc86159c9a47de97e
gezcarlos22 <ceg290698@gmail.com>
2025-07-30 23:01:06 -0300

    Comienzo del proyecto

---
+++ src/main/java/com/gezcarlos/Literatura/LiteraturaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.gezcarlos.Literatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturaBackEndApplication.class, args);
+	}
+
+}
