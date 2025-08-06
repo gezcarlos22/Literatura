120c5860dc6f8536f3d6549bb96d83a36be32064
gezcarlos22 <ceg290698@gmail.com>
2025-08-06 02:25:51 -0300

    Resolucion Final

--- src/main/java/com/gezcarlos/Literatura/LiteraturaBackEndApplication.java
+++ src/main/java/com/gezcarlos/Literatura/LiteraturaBackEndApplication.java
@@ -1,30 +1,23 @@
 package com.gezcarlos.Literatura;
 
 import com.gezcarlos.Literatura.principal.Principal;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-
-
-@SpringBootApplication
-public class LiteraturaBackEndApplication implements CommandLineRunner{
-
-	@Autowired
-	Principal principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LiteraturaBackEndApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiteraturaBackEndApplication.class, args);
+	@SpringBootApplication
+	public class LiteraturaBackEndApplication implements CommandLineRunner {
+		@Autowired
+		Principal principal;
+
+		public static void main(String[] args) {
+			SpringApplication.run(LiteraturaBackEndApplication.class, args);
+		}
+
+		@Override
+		public void run(String... args) throws Exception {
+			principal.muestraMenu();
+		}
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		principal.muestraMenu();
-	}
-
-}
