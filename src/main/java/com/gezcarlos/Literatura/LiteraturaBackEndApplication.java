f2eedbb7e9c81f2c8a5cfb2f39f7adcefcf0df01
gezcarlos22 <ceg290698@gmail.com>
2025-08-06 02:18:23 -0300

    Resuelto conflicto de fusión

--- src/main/java/com/gezcarlos/Literatura/LiteraturaBackEndApplication.java
+++ src/main/java/com/gezcarlos/Literatura/LiteraturaBackEndApplication.java
@@ -1,6 +1,5 @@
 package com.gezcarlos.Literatura;
 
-<<<<<<< HEAD
 import com.gezcarlos.Literatura.principal.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,23 +12,19 @@ public class LiteraturaBackEndApplication implements CommandLineRunner{
 
 	@Autowired
 	Principal principal;
-=======
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiteraturaBackEndApplication {
->>>>>>> e65ddec91a6f3d4754b0b55cc86159c9a47de97e
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraturaBackEndApplication.class, args);
 	}
 
-<<<<<<< HEAD
 	@Override
 	public void run(String... args) throws Exception {
 		principal.muestraMenu();
 	}
-=======
->>>>>>> e65ddec91a6f3d4754b0b55cc86159c9a47de97e
+
 }
